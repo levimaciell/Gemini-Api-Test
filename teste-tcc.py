9b467f65e2d89c1bbc6a04e490e169c8af63fe7e
levimaciell <levimaciell1221@gmail.com>
2025-11-26 08:39:30 -0300

    fixing api key leak lol

--- teste-tcc.py
+++ teste-tcc.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key="AIzaSyDTCyZI9i3Omx6aw3BfwdXYujbrkMLfhFM")
+client = genai.Client()
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
