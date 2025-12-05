a80df4e865d3c517330f47975781ca18b6a81699
levimaciell <levimaciell1221@gmail.com>
2025-12-05 15:35:13 -0300

    sript

---
+++ exec_sast.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+bandit -r "Gemini-Api-Test/vulnerable_files/files" -f json -o results/bandit.json
+semgrep scan "Gemini-Api-Test/vulnerable_files/files" --json --json-output=results/semgrep.json
+
+python Gemini-Api-Test/bandit_parser.py -i results/bandit.json -o results/formatted_bandit.json
+python Gemini-Api-Test/semgrep_parser.py -i results/semgrep.json -o results/formatted_semgrep.json
+python Gemini-Api-Test/unify.py -d /app/results -o fused.json
