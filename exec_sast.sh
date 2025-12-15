7791d44e0015e6ad9510efbb333c64943ab4f49d
levimaciell <levimaciell1221@gmail.com>
2025-12-15 16:22:01 -0300

    changing stuff

--- exec_sast.sh
+++ exec_sast.sh
@@ -5,4 +5,4 @@ semgrep scan "Gemini-Api-Test/vulnerable_files/files" --json --json-output=resul
 
 python Gemini-Api-Test/bandit_parser.py -i results/bandit.json -o results/formatted_bandit.json
 python Gemini-Api-Test/semgrep_parser.py -i results/semgrep.json -o results/formatted_semgrep.json
-python Gemini-Api-Test/unify.py -d /app/results -o fused.json
+python Gemini-Api-Test/unify.py -d /app/results -o results/fused.json
